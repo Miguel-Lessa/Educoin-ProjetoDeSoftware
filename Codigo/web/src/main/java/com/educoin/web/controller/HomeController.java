0b2365f6def2eea9989116e52b81892c0ffb2278
Miguel-Lessa <meu1emeio1soh1meu@gmail.com>
2025-05-17 03:40:45 -0300

    css in some screens and homepage
    I'll finish later today, it's almost 4am and I need to sleep

--- Codigo/web/src/main/java/com/educoin/web/controller/HomeController.java
+++ Codigo/web/src/main/java/com/educoin/web/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.educoin.web.controller;
 
+import com.educoin.web.model.Aluno;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,4 +11,5 @@ public class HomeController {
     public String home() {
         return "home";
     }
+
 }
--- Codigo/web/src/main/java/com/educoin/web/controller/LoginController.java
+++ Codigo/web/src/main/java/com/educoin/web/controller/LoginController.java
@@ -1,12 +1,29 @@
 package com.educoin.web.controller;
 
+import com.educoin.web.model.Aluno;
+import com.educoin.web.model.EmpresaParceira;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
+
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping("/aluno-login")
+    public String alunoLogin(Model model) {
+        model.addAttribute("aluno", new Aluno());
+        return "aluno-login";
+    }
+
+    @GetMapping("/empresa-login")
+    public String empresaLogin(Model model) {
+        model.addAttribute("empresa", new EmpresaParceira());
+        return "empresa-login";
     }
 }
